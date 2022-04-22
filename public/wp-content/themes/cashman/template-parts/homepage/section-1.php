a2059ad579c36f0bfa42aa8d3ec324ecbb160d21
Garrett Cullen <Garth619@users.noreply.github.com>
2022-04-22 16:44:28 -0700

    g$

--- public/wp-content/themes/cashman/template-parts/homepage/section-1.php
+++ public/wp-content/themes/cashman/template-parts/homepage/section-1.php
@@ -1,3 +1,18 @@
 <section id='section-one'>
-
+    <div id='sec-one-inner'>
+        <ul id='sec-one-sp'>
+            <li>TrUST</li>
+            <li>INTEGRITY</li>
+            <li>EXCELLENCE</li>
+        </ul>
+        <span id='sec-one-title'>
+            <span class='brown-light'>PeRSONALIZED SERVICE</span> IS AT THE HEART OF WHAT WE DO.
+        </span>
+        <a class='button-one' href='#consultation'>Click for free case evaluation</a>
+    </div>
+    <picture>
+        <!-- <source media='(min-width: 1170px)' srcset='' type='image/webp'>
+        <source media='(min-width: 1380px)' srcset=''> -->
+        <img id='hero' src='<?php bloginfo('template_directory'); ?>/images/header_img_mobile.jpg' alt='' width='768' height='607' loading='eager' />
+    </picture>
 </section>
