03d2de813afb753e7b17c4ab238871b2bd60c5f3
Garrett Cullen <Garth619@users.noreply.github.com>
2022-04-27 18:15:08 -0700

    g$

--- public/wp-content/themes/cashman/footer.php
+++ public/wp-content/themes/cashman/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div id='footer-inner'>
         <div id='consultation'>
-            <span id='footer-form-title'>You are our priority</span>
+            <span id='footer-form-title'>You are<br> our priority</span>
             <span id='footer-form-subtitle'>Free Case Evaluation</span>
             <span id='footer-form-descrip'>Rest assured a member of The Cashman Law Firm will be in touch with you shortly.</span>
             <?php gravity_form(1, false, false, false, '', true, 2323); ?>
@@ -14,14 +14,14 @@
                 <span class='footer-title'>SaLEM OFFICE</span>
                 <span class='footer-address'>27 Congress Street, # 401 Salem, MA 01970</span>
                 <a class='get-directions' href='' target='_blank' rel='noopener'>Get Directions</a>
-                <a href='tel:+18002003120'><span>P</span> (800) 200-3120</a>
+                <a href='tel:+18002003120'><span class='p-accent'>P</span> (800) 200-3120</a>
             </div>
             <div class='footer-info-col'>
                 <span class='footer-title'>BOSTON OFFICE</span>
                 <span class='footer-address'>27 Congress Street, # 401
                     Salem, MA 01970</span>
                 <a class='get-directions' href='' target='_blank' rel='noopener'>Get Directions</a>
-                <a href='tel:+18002003120'><span>P</span> (800) 200-3120</a>
+                <a href='tel:+18002003120'><span class='p-accent'>P</span> (800) 200-3120</a>
             </div>
             <div class='footer-info-col'>
                 <div id='social-media'>
@@ -37,10 +37,8 @@
                 </div>
             </div>
         </div>
+        <img id='footer-bg' src='<?php bloginfo('template_directory'); ?>/images/bg.jpg' alt='altname' width='1943' height='1497' loading='lazy' />
     </div>
-
-
-
 </footer>
 <?php wp_footer(); ?>
 <?php the_field('footer_scripts', 'option'); ?>
