2997e6778fb489f2505c6375c98829b6fecccea5
Garrett Cullen <Garth619@users.noreply.github.com>
2022-05-03 12:27:37 -0700

    push

--- public/wp-content/themes/cashman/template-parts/template-testimonials-part.php
+++ public/wp-content/themes/cashman/template-parts/template-testimonials-part.php
@@ -1,14 +1,23 @@
 <?php
 if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div id='testimonials-wrapper' class='template-part-wrapper'>
-            <div id='testimonials-feature'>
+            <div id='testimonials-feature' class='testimonials-box'>
                 <img class='stars' src='<?php bloginfo('template_directory'); ?>/images/stars_1.svg' alt='Stars Icon' width='157' height='26' />
                 <span class='intro'>The most comforting part was how easily I could reach him. Mark responded to my emails and calls promptly I would highly recommend him.</span>
-                <p>My name is Marianne I am a RN who was heading home from a 12 hr shift working in the Er I was stopped at a red light waiting to turn when I was hit head on by a truck . I had a displaced sternal fracture which kept me out of work for 3 1/2 months I had Mark represent me thru a very complicated legal process…</p>
+                <div class='content'>
+                    <p>My name is Marianne I am a RN who was heading home from a 12 hr shift working in the Er I was stopped at a red light waiting to turn when I was hit head on by a truck . I had a displaced sternal fracture which kept me out of work for 3 1/2 months I had Mark represent me thru a very complicated legal process…</p>
+                </div>
                 <span class='name'>MaRIANNE - CLIENT</span>
             </div>
             <div id='testimonials-list'>
-
+                <div class='testimonials-single testimonials-box'>
+                    <img class='stars' src='<?php bloginfo('template_directory'); ?>/images/stars_1.svg' alt='Stars Icon' width='157' height='26' />
+                    <span class='intro'>The most comforting part was how easily I could reach him. Mark responded to my emails and calls promptly I would highly recommend him.</span>
+                    <div class='content'>
+                        <p>My name is Marianne I am a RN who was heading home from a 12 hr shift working in the Er I was stopped at a red light waiting to turn when I was hit head on by a truck . I had a displaced sternal fracture which kept me out of work for 3 1/2 months I had Mark represent me thru a very complicated legal process…</p>
+                    </div>
+                    <span class='name'>MaRIANNE - CLIENT</span>
+                </div>
             </div>
 
         </div>
