f438c27d10b38b3d016a502f85b7a692c2356e9d
Garrett Cullen <Garth619@users.noreply.github.com>
2022-05-06 18:00:18 -0700

    push

--- public/wp-content/themes/cashman/template-parts/video.php
+++ public/wp-content/themes/cashman/template-parts/video.php
@@ -7,12 +7,15 @@
  */
 $array_defaults = array(
     'acf-field' => '',
+    'width' => '500',
 );
 $args = wp_parse_args($args, $array_defaults);
 $acf_field = $args['acf-field'];
+$width = $args['width'];
 /**
  * ACFs
  */
+$title = $acf_field['video_options_video_title'];
 $video_choice = $acf_field['wistia_or_youtube'];
 $wistia = $video_choice; // !$wistia is youtube
 $wistiaID = $acf_field['video_option_wistia_id'];
@@ -21,50 +24,37 @@ $youtube_image = 'https://img.youtube.com/vi/' . $youtubeID . '/0.jpg';
 $thumbnail =  $acf_field['video_options_thumbnail'];
 $thumbnail_url = $thumbnail['url'];
 $thumbnail_alt = $thumbnail['alt'];
-$title = $acf_field['video_options_video_title'];
+/**
+ * Get Wistia thumbnail - https://wistia.com/support/developers/working-with-images
+ */
 $ch = curl_init();
-
-// set url
-curl_setopt($ch, CURLOPT_URL, "fast.wistia.net/oembed?url=http://home.wistia.com/medias/dsqtgjeu3h?embedType=async&videoWidth=473");
-
-//return the transfer as a string
+curl_setopt($ch, CURLOPT_URL, 'fast.wistia.net/oembed?url=http://home.wistia.com/medias/' . $wistiaID . '?embedType=async&videoWidth=' . $width);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-// $output contains the output string
 $output = curl_exec($ch);
-
 $response = json_decode($output, true);
-//$errors = $response['response']['errors'];
-$data = $response['thumbnail_url'];
-
-print_r($data);
-// close curl resource to free up system resources
+$wistia_thumbnail = $response['thumbnail_url'];
+$wistia_alt = $response['title'];
 curl_close($ch);
-if ($wistia) {
-    $videolink = '#';
-    if ($thumbnail) {
-        $view = 'html';
-    } else {
-        $view = 'thumbnail';
-    }
+// can these be switch cases
+if ($thumbnail) {
+    $src = $thumbnail_url;
+    $alt = $thumbnail_alt;
 }
-if (!$wistia) {
-    $videolink = 'https://www.youtube.com/embed/' . $youtubeID;
+if (!$thumbnail && $wistia) {
+    $src = $wistia_thumbnail;
+    $alt = $wistia_alt . ' Thumbnail';
 }
-
-?>
+if (!$thumbnail && !$wistia) {
+    $src = $youtube_image;
+    $alt = 'Youtube Video Thumbnail';
+} ?>
 <div class='single-video'>
-    <a href='<?= $videolink; ?>' <?= !$wistia ? 'data-lity' : ''; ?>>
+    <<?= $wistia ? 'div' : 'a href="https://www.youtube.com/embed/' . $youtubeID . '" data-lity'; ?>>
         <div class='single-video-thumbnail-wrapper'>
-            <?php if ($thumbnail) { ?>
-                <img class='single-video-thumbnail' src='<?= $thumbnail_url; ?>' alt='<?= $thumbnail_alt; ?>' />
-            <?php }
-            if (!$thumbnail && !$wistia) { ?>
-                <img class='single-video-thumbnail' src="<?= $youtube_image; ?>" />
-            <?php } ?>
-            <img class='single-video-spacer' src='<?php bloginfo('template_directory'); ?>/images/spacer.png' alt='Image Spacer' />
+            <img class='single-video-thumbnail' src='<?= $src; ?>' alt='<?= $alt; ?>' />
+            <img class='single-video-spacer' src='<?php bloginfo('template_directory'); ?>/images/spacer.png' width='<?= $width; ?>' height='' alt='Image Spacer' />
             <?php if ($wistia) { ?>
-                <div data-wistia='<?= $wistiaID; ?>' class='single-wistia wistia_embed wistia_async_<?= $wistiaID; ?> popover=true popoverContent=<?= $view; ?>'></div>
+                <div data-wistia='<?= $wistiaID; ?>' class='single-wistia wistia_embed wistia_async_<?= $wistiaID; ?> popover=true popoverContent=html'></div>
             <?php } ?>
             <div class='single-video-overlay'>
                 <span class='single-video-play-button'><span></span></span>
@@ -73,8 +63,5 @@ if (!$wistia) {
         <?php if ($title) { ?>
             <span class='single-video-title'><?= $title; ?></span>
         <?php } ?>
-    </a>
+    </<?= $wistia ? 'div' : 'a'; ?>>
 </div>
-<?php
-
-?>
